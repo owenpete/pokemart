42a8eb625797a73f31441c420158908e1a916f16
ogpea <ogpete23@gmail.com>
2021-07-08 21:24:18 -0500

    Fixed: cart now displays items in the order of which they were added

--- pages/api/cart/index.ts
+++ pages/api/cart/index.ts
@@ -1,17 +1,34 @@
-import connectStore from '../../../server/connectStore';
-import ProductSchema from '../../../schemas/ProductSchema';
-import { getCart } from '../../../utils/cart';
+import connectStore from "../../../services/connectStore";
+import ProductSchema from "../../../schemas/ProductSchema";
+import { getCart } from "../../../utils/cart";
 
-export default async function(req: any, res: any){
-  const { ids } = req.query;
-  const cartIds = JSON.parse(ids);
-  try{
+const sortData = (products, ids: any) =>{
+  //sorts data in order of items in cart
+  return(Object.values(ids).map((value: any)=>{
+    for(let i = 0; i < products.length; i++){
+      if(products[i].id == value.id){
+        return(
+          products[i]
+        )
+      }
+    }
+  }));
+}
+
+export default async function (req: any, res: any) {
+  //product ids from localStorage
+  const ids = JSON.parse(req.query.ids);
+  try {
     const db = await connectStore();
     const product = db.model("Product", ProductSchema);
-    const data = await product.where('id').in(Object.values(cartIds).map((value: any)=>value.id));
-    res.status(200).json({ data })
-  }catch(err: any){
-    console.log(err)
-    res.status(500).json({err: err});
+    //finding products in database that match localStorage ids
+    const data = await product
+      .where("id")
+      .in(Object.values(ids).map((value: any) => value.id));
+    const sortedData = sortData(data, ids);
+    res.status(200).json({ data: sortedData});
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).json({ err: err });
   }
 }
