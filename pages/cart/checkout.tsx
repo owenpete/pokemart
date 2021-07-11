9796bf318cf40c20d033fa8867c2e92f3f66c439
ogpea <ogpete23@gmail.com>
2021-07-11 18:18:21 -0500

    Added: checkout page

---
+++ pages/cart/checkout.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Checkout(){
+  return(
+    <div className='checkout'>
+    </div>
+  );
+}
