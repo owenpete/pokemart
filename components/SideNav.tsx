c494ddbc8ad32549a566ef707b3a2237f20e0e7a
ogpea <ogpete23@gmail.com>
2021-07-23 20:07:26 -0500

    Added: SideNav component

---
+++ components/SideNav.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+const SideNav  = () =>{
+
+  return (
+    <div className='sidnav'>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default SideNav;
