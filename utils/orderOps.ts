cad8e16c191f112aa5192eef4c70a2612c4f5b0d
ogpea <ogpete23@gmail.com>
2021-08-07 17:34:30 -0500

    chore: add order operation methods

---
+++ utils/orderOps.ts
@@ -0,0 +1,22 @@
+const orderStorageName = 'orders';
+
+export const getOrders = () =>{
+  try{
+    const orders = window.localStorage.getItem(orderStorageName);
+    return JSON.parse(orders);
+  }catch(err: any){
+    console.log(err)
+  }
+}
+
+export const addOrder = (orderId: string) =>{
+  const orders = getOrders();
+  const orderObject = {
+    ...orders,
+    [orderId]: {
+      orderId: orderId
+    }
+  }
+  window.localStorage.setItem(orderStorageName, orderObject);
+}
+
