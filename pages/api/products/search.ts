4bb91e3babe6a04c401fbbea8433b40f04a76739
owenpete <owengpete@gmail.com>
2021-09-14 21:11:11 -0500

    fix: item search throws an error when not given a query string

--- pages/api/products/search.ts
+++ pages/api/products/search.ts
@@ -5,22 +5,38 @@ export default async function (req: any, res: any) {
   const db = await connectStore();
   const { q, skip, limit } = req.query;
   const searchModel = db.model('product', ProductSchema);
-  const data = await searchModel.find({
-    $text: {
-      $search: q,
-      $caseSensitive: false
-    }
-  })
-  .sort({ score: { $meta: 'textScore' } })
-  .skip(parseInt(skip))
-  .limit(parseInt(limit));
-  const totalResults = await searchModel.find({
-    $text: {
-      $search: q,
-      $caseSensitive: false
-    }
-  })
-  .countDocuments();
+  let data:  any;
+  let totalResults: number;
+  if(q != null){
+    data = await searchModel.find({
+      $text: {
+        $search: q,
+        $caseSensitive: false
+      }
+    })
+    .sort({ score: { $meta: 'textScore' } })
+    .skip(parseInt(skip))
+    .limit(parseInt(limit));
+    totalResults = await searchModel.find({
+      $text: {
+        $search: q,
+        $caseSensitive: false
+      }
+    })
+    .countDocuments();
+  }else{
+    data = await searchModel.find({
+    })
+    .sort({
+      nos: 'asc'
+    })
+    .skip(parseInt(skip))
+    .limit(parseInt(limit));
+    totalResults = await searchModel.find({
+    })
+    .countDocuments();
+  }
+
   res.status(200).json({
     data: data,
     totalResults: totalResults,
